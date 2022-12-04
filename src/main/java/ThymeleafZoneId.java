5923618b4cd7b5bdddaa9c6910f367054d9f262b
dmitryaushev <dmitryaushev@gmail.com>
2022-12-04 20:26:28 +0200

    Refactored

--- src/main/java/ThymeleafZoneId.java
+++ src/main/java/ThymeleafZoneId.java
@@ -14,63 +14,59 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+
 @WebServlet(value = "/time")
 public class ThymeleafZoneId extends HttpServlet {
-    static final String FORMAT_DATA_TIME = "dd.MM.yyyy HH:mm:ss";
+    private final String FORMAT_DATA_TIME = "dd.MM.yyyy HH:mm:ss";
+    private final String TIMEZONE_COOKIE_KEY = "lastTimezone";
     private TemplateEngine engine;
+
+    private Properties properties = PropertiesUtil.loadProperties("application.properties");
+
     @Override
     public void init() {
         engine = new TemplateEngine();
 
         FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setPrefix("C:\\Users\\Admin\\IdeaProjects\\JavaDeveloperGOIT\\ServletZoneId\\templates\\");
-        resolver.setSuffix(".html");
-        resolver.setTemplateMode("HTML5");
+        resolver.setPrefix(properties.getProperty("prefix"));
+        resolver.setSuffix(properties.getProperty("suffix"));
+        resolver.setTemplateMode(properties.getProperty("template"));
         resolver.setOrder(engine.getTemplateResolvers().size());
         resolver.setCacheable(false);
         engine.addTemplateResolver(resolver);
     }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
+
         String timezone = req.getParameter("timezone");
-        Cookie cookie = new Cookie("lastTimezone", timezone);
-        if(timezone!=null){
-            resp.addCookie(cookie);
+        if (timezone != null) {
+            CookieUtil.addCookie(TIMEZONE_COOKIE_KEY, timezone, resp);
         }
+
+        String outputTime = parseTime(req, timezone);
+
         Context simpleContext = new Context(req.getLocale(),
-                Map.of("time", parseTime(req,timezone)));
+                Map.of("time", outputTime));
         engine.process("test", simpleContext, resp.getWriter());
+
         resp.getWriter().close();
     }
-    private String parseTime(HttpServletRequest req, String timezone) {
-        Map<String, String> cookies = getCookies(req);
 
-        if(cookies.containsKey("lastTimezone") && timezone==null){
-            timezone=cookies.get("lastTimezone");
+    private String parseTime(HttpServletRequest req, String timezone) {
+        String lastTimezone = CookieUtil.getCookie("lastTimezone", req);
+
+        if (lastTimezone != null && timezone == null) {
+            timezone = lastTimezone;
+        } else if (timezone == null) {
+            timezone = "UTC";
         }
-        if (timezone!=null) {
-            ZoneId zoneId = ZoneId.of(timezone);
-            ZonedDateTime zdt = ZonedDateTime.now(zoneId);
-            String timeZoneUtc = zdt.format(DateTimeFormatter.ofPattern(FORMAT_DATA_TIME));
-            return timeZoneUtc + " " + timezone;
-        }
-        ZoneId zoneId = ZoneId.of("UTC");
+
+        ZoneId zoneId = ZoneId.of(timezone);
         ZonedDateTime zdt = ZonedDateTime.now(zoneId);
-        String utc = zdt.format(DateTimeFormatter.ofPattern(FORMAT_DATA_TIME));
-        return utc + " UTC";
-    }
-    private Map<String, String> getCookies(HttpServletRequest req) {
-        String cookies = req.getHeader("Cookie");
-        if (cookies == null) {
-            return Collections.emptyMap();
-        }
-        Map<String, String> result = new HashMap<>();
-        String[] separateCookies = cookies.split(";");
-        for (String pair : separateCookies) {
-            String[] keyValue = pair.split("=");
-            result.put(keyValue[0], keyValue[1]);
-        }
-        return result;
+        String outputTime = zdt.format(DateTimeFormatter.ofPattern(FORMAT_DATA_TIME));
+        return outputTime + " " + timezone;
     }
 }
